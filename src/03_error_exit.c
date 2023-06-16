61ed1954a756d6ab8e3914584921b2e487327b99
Jeremy Hurpy <jeremyhurpy@gmail.com>
2023-06-17 05:25:19 +0700

    remove error explanation

--- src/03_error_exit.c
+++ src/03_error_exit.c
@@ -6,7 +6,7 @@
 /*   By: jhurpy <jhurpy@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/08 22:31:56 by jhurpy            #+#    #+#             */
-/*   Updated: 2023/06/09 01:04:33 by jhurpy           ###   ########.fr       */
+/*   Updated: 2023/06/17 05:01:44 by jhurpy           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,6 @@ void	error_malloc(char *array)
 {
 	free(array);
 	ft_putstr_fd("Error\n", 2);
-	ft_putstr_fd("Malloc error: fail allocation\n", 2);
 	exit(1);
 }
 
@@ -24,7 +23,6 @@ void	error_digit(char **array)
 {
 	free_2d_array(array);
 	ft_putstr_fd("Error\n", 2);
-	ft_putstr_fd("Usage: Input must be valid digits\n", 2);
 	exit(1);
 }
 
@@ -33,7 +31,6 @@ void	error_integers(char **array, t_stack *stack)
 	free_2d_array(array);
 	free_stack(stack);
 	ft_putstr_fd("Error\n", 2);
-	ft_putstr_fd("Usage: Input must be integers\n", 2);
 	exit(1);
 }
 
@@ -42,6 +39,5 @@ void	error_double(char **array, t_stack *stack)
 	free_2d_array(array);
 	free_stack(stack);
 	ft_putstr_fd("Error\n", 2);
-	ft_putstr_fd("Usage: Input must be unique\n", 2);
 	exit(1);
 }
